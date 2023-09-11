71d3772352b3661ccf014a7a94dc73b185a3ff63
tuomijan9 <74894836+tuomijan9@users.noreply.github.com>
2023-09-11 10:02:07 +0300

    updated language

--- Support-scripts/Set-KeyboardLanguage.ps1
+++ Support-scripts/Set-KeyboardLanguage.ps1
@@ -1,12 +1,12 @@
 $Global:Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-Set-KeyboardLanguage.log"
 Start-Transcript -Path (Join-Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" $Global:Transcript) -ErrorAction Ignore
 
-Write-Host -ForegroundColor Green "Set keyboard language to de-CH"
+Write-Host -ForegroundColor Green "Set keyboard language to fi-FI"
 Start-Sleep -Seconds 5
 
 $LanguageList = Get-WinUserLanguageList
 
-$LanguageList.Add("de-CH")
+$LanguageList.Add("fi-FI")
 Set-WinUserLanguageList $LanguageList -Force
 
 Start-Sleep -Seconds 5
